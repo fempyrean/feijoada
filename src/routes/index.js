75e5baec8b4524049fdada51185ea31cb138d3fb
Lucas Sousa <frempyrean@gmail.com>
2020-02-23 18:57:09 -0300

    Iniciando navegação

---
+++ src/routes/index.js
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+/**
+ * Estrutura de navegação do App.
+ */
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+    createStackNavigator,
+    CardStyleInterpolators,
+} from '@react-navigation/stack';
+
+/** Importação das telas */
+import Home from '../screens/Home/Home';
+
+/** Criando Stack. Navigators que sejam uma stack utilizarão Stack.Navigator e Stack.Screen */
+const Stack = createStackNavigator();
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Splash"
+                headerMode="none"
+                screenOptions={{
+                    gestureEnabled: false,
+                    cardStyleInterpolator:
+                        CardStyleInterpolators.forHorizontalIOS,
+                }}
+            >
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
